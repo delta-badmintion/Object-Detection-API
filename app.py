ac0c679dace43a7420877d46197182b54f966d25
Devakanta Rao <dev681999@gmail.com>
2020-09-15 11:22:11 +0530

    modify API

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ import os
 
 # customize your API through the following parameters
 classes_path = './data/labels/coco.names'
+# weights_path = './weights/yolov3-tiny.tf'
+# tiny = True                    # set to True if using a Yolov3 Tiny model
 weights_path = './weights/yolov3.tf'
 tiny = False                    # set to True if using a Yolov3 Tiny model
 size = 416                      # size images are resized to for model
-output_path = './detections/'   # path to output folder where images with detections are saved
+# path to output folder where images with detections are saved
+output_path = './detections/'
 num_classes = 80                # number of classes in model
 
 # load in weights and classes
@@ -39,6 +42,8 @@ print('classes loaded')
 app = Flask(__name__)
 
 # API that returns JSON with classes found in images
+
+
 @app.route('/detections', methods=['POST'])
 def get_detections():
     raw_images = []
@@ -51,9 +56,9 @@ def get_detections():
         img_raw = tf.image.decode_image(
             open(image_name, 'rb').read(), channels=3)
         raw_images.append(img_raw)
-        
+
     num = 0
-    
+
     # create list for final response
     response = []
 
@@ -61,7 +66,7 @@ def get_detections():
         # create list of responses for current image
         responses = []
         raw_img = raw_images[j]
-        num+=1
+        num += 1
         img = tf.expand_dims(raw_img, 0)
         img = transform_images(img, size)
 
@@ -70,15 +75,27 @@ def get_detections():
         t2 = time.time()
         print('time: {}'.format(t2 - t1))
 
+        img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+        wh = np.flip(img.shape[0:2])
+
         print('detections:')
         for i in range(nums[0]):
-            print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                            np.array(scores[0][i]),
-                                            np.array(boxes[0][i])))
-            responses.append({
-                "class": class_names[int(classes[0][i])],
-                "confidence": float("{0:.2f}".format(np.array(scores[0][i])*100))
-            })
+            if int(classes[0][i]) == 0:
+                x1y1 = ((np.array(boxes[0][i][0:2]) * wh).astype(np.int32))
+                x2y2 = ((np.array(boxes[0][i][2:4]) * wh).astype(np.int32))
+
+                print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                            x1y1,
+                                            x2y2))
+
+                responses.append({
+                    "class": class_names[int(classes[0][i])],
+                    "confidence": float("{0:.2f}".format(np.array(scores[0][i])*100)),
+                    "x1": float(x1y1[0]),
+                    "y1": float(x1y1[1]),
+                    "x2": float(x2y2[0]),
+                    "y2": float(x2y2[1]),
+                })
         response.append({
             "image": image_names[j],
             "detections": responses
@@ -86,18 +103,21 @@ def get_detections():
         img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         cv2.imwrite(output_path + 'detection' + str(num) + '.jpg', img)
-        print('output saved to: {}'.format(output_path + 'detection' + str(num) + '.jpg'))
+        print('output saved to: {}'.format(
+            output_path + 'detection' + str(num) + '.jpg'))
 
-    #remove temporary images
+    # remove temporary images
     for name in image_names:
         os.remove(name)
     try:
-        return jsonify({"response":response}), 200
+        return jsonify({"response": response}), 200
     except FileNotFoundError:
         abort(404)
 
 # API that returns image with detections on it
-@app.route('/image', methods= ['POST'])
+
+
+@app.route('/image', methods=['POST'])
 def get_image():
     image = request.files["images"]
     image_name = image.filename
@@ -115,23 +135,25 @@ def get_image():
     print('detections:')
     for i in range(nums[0]):
         print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                        np.array(scores[0][i]),
-                                        np.array(boxes[0][i])))
+                                    np.array(scores[0][i]),
+                                    np.array(boxes[0][i])))
     img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
     img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
     cv2.imwrite(output_path + 'detection.jpg', img)
     print('output saved to: {}'.format(output_path + 'detection.jpg'))
-    
+
     # prepare image for response
     _, img_encoded = cv2.imencode('.png', img)
     response = img_encoded.tostring()
-    
-    #remove temporary image
+
+    # remove temporary image
     os.remove(image_name)
 
     try:
         return Response(response=response, status=200, mimetype='image/png')
     except FileNotFoundError:
         abort(404)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
